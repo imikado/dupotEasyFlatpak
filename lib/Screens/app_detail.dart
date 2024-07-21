f63096d08000a6c08d7955c8171f82d78d171a32
mika <imikado@gmail.com>
2024-07-21 19:07:53 +0200

    read recipie from json

--- lib/Screens/app_detail.dart
+++ lib/Screens/app_detail.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class AppDetail extends StatefulWidget {
@@ -13,11 +15,25 @@ class AppDetail extends StatefulWidget {
 }
 
 class _AppDetail extends State<AppDetail> {
+  Application application = Application("loading", "loading");
+  bool loaded = false;
+
+  void getData(BuildContext context, app) async {
+    Application newApp = await getApplication(context, app);
+    setState(() {
+      application = newApp;
+      loaded = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as AppDetailArguments;
 
+    if (!loaded) {
+      getData(context, args.app);
+    }
     return Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
@@ -34,7 +50,10 @@ class _AppDetail extends State<AppDetail> {
             ),
           ],
         ),
-        body: Column(children: [Text("mon app:" + args.app)]));
+        body: Column(children: [
+          Text("mon app:" + application.title),
+          Text("Description:" + application.description)
+        ]));
   }
 }
 
@@ -44,4 +63,22 @@ class AppDetailArguments {
   AppDetailArguments(
     String this.app,
   );
+}
+
+Future<Application> getApplication(context, app) async {
+  String applicaitonRecipieString = await DefaultAssetBundle.of(context)
+      .loadString("assets/recipies/" + app + ".json");
+  print(applicaitonRecipieString);
+
+  Map jsonApp = json.decode(applicaitonRecipieString);
+  return Application(jsonApp['title'], jsonApp['description']);
+
+  return Application("test", "test");
+}
+
+class Application {
+  final String title;
+  final String description;
+
+  Application(String this.title, String this.description);
 }
