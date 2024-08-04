83e2e6287aa1e626c9c74876bf87c7dc0b149917
mika <imikado@gmail.com>
2024-08-04 14:55:22 +0200

    upd test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dupot_easy_flatpak/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:dupot_easy_flatpak/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(DupotEasyFlatpak());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
