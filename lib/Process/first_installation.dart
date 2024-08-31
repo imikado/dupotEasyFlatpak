2fc90be8c65c98e516a337f525932f9c8b4f9f9b
mika <imikado@gmail.com>
2024-08-31 14:28:22 +0200

    fix installation

--- lib/Process/first_installation.dart
+++ lib/Process/first_installation.dart
@@ -6,18 +6,32 @@ import 'package:path/path.dart' as p;
 import 'dart:io';
 
 class FirstInstallation {
-  Future<void> copyTo(String fromPath, String targetPath) async {
-    Process.run('cp', [fromPath, targetPath]);
+  Future<void> mkdir(String path) async {
+    var result = Process.runSync('/usr/bin/mkdir', [path]);
+    print(result.stdout);
+    print(result.stderr);
+
+    await cwd();
   }
 
-  Future<void> copyRecrusiveTo(String fromPath, String targetPath) async {
-    Process.run('cp', ['-R', fromPath, targetPath]);
+  Future<void> cwd() async {
+    var result = Process.runSync('/usr/bin/pwd', []);
+    print(result.stdout);
+    print(result.stderr);
+  }
+
+  Future<void> copyTo(String fromPath, String targetPath) async {
+    var result = Process.runSync('/usr/bin/cp', [fromPath, targetPath],
+        runInShell: true);
+    print(result.stdout);
+    print(result.stderr);
   }
 
   Future<void> unarchive(String archivePath, String targetPath) async {
-    var result =
-        await Process.run('tar', ['-xvzf', archivePath, '-C', targetPath]);
+    var result = Process.runSync(
+        '/usr/bin/tar', ['-xvzf', archivePath, '-C', targetPath]);
     print(result.stdout);
+    print(result.stderr);
   }
 
   Future<void> process() async {
@@ -29,15 +43,33 @@ class FirstInstallation {
         io.Directory(p.join(appDocumentsDirPath, "EasyFlatpak"));
 
     if (!documentsTargetDirectory.existsSync()) {
-      documentsTargetDirectory.createSync();
+      await mkdir(documentsTargetDirectory.path);
 
-      await copyTo('assets/db/flathub_database.db',
+      await copyAssetTo('assets/db/flathub_database.db',
+          '${documentsTargetDirectory.path}/flathub_database.db');
+
+      String targetIconsArchive =
+          '${documentsTargetDirectory.path}/Archive.tar.gz';
+
+      await copyAssetTo('assets/icons/Archive.tar.gz', targetIconsArchive);
+
+      await unarchive(targetIconsArchive, documentsTargetDirectory.path);
+
+/*
+      await copyTo('./assets/db/flathub_database.db',
           '${documentsTargetDirectory.path}/flathub_database.db');
 
       await unarchive(
-          'assets/icons/Archive.tar.gz', documentsTargetDirectory.path);
+          './assets/icons/Archive.tar.gz', documentsTargetDirectory.path);
+          */
     } else {
       print('already installed');
     }
+  }
+
+  Future<void> copyAssetTo(String assetPath, String targetPath) async {
+    final bytes = await rootBundle.load(assetPath);
+    final targetFile = File(targetPath);
+    await targetFile.writeAsBytes(bytes.buffer.asUint8List());
   }
 }
