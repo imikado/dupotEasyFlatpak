f13d3132cccc55d3ebf03dba966b4ba10840fc33
imikado <imikado@gmail.com>
2025-10-10 23:55:57 +0200

    upd

--- lib/Infrastructure/Screen/View/home_view.dart
+++ lib/Infrastructure/Screen/View/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:dupot_easy_flatpak/Domain/Entity/db/application_entity.dart';
+import 'package:dupot_easy_flatpak/Infrastructure/Api/localization_api.dart';
 import 'package:dupot_easy_flatpak/Infrastructure/Control/Model/View/home_view_model.dart';
 import 'package:dupot_easy_flatpak/Infrastructure/Screen/SharedComponents/Group/block_app_list_component.dart';
 import 'package:flutter/material.dart';
@@ -48,6 +49,32 @@ class _HomeViewState extends State<HomeView> {
     });
   }
 
+  Widget getWait(String title) {
+    return Column(children: [
+      const SizedBox(height: 12),
+      Row(
+        children: [
+          SizedBox(
+            width: 30,
+          ),
+          Text(
+            LocalizationApi().tr(title),
+            style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey),
+          ),
+        ],
+      ),
+      const SizedBox(height: 10),
+      SizedBox(
+        width: 100,
+        height: 100,
+        child: CircularProgressIndicator(),
+      )
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scrollbar(
@@ -56,21 +83,27 @@ class _HomeViewState extends State<HomeView> {
         controller: scrollController,
         scrollbarOrientation: ScrollbarOrientation.right,
         child: ListView(controller: scrollController, children: [
-          BlockAppListComponent(
-              categoryId: 'home_trending',
-              appStreamList: stateTrendingAppStreamList,
-              appPath: appPath,
-              handleGoTo: widget.handleGoTo),
-          BlockAppListComponent(
-              categoryId: 'home_updated',
-              appStreamList: stateUpdatedAppStreamList,
-              appPath: appPath,
-              handleGoTo: widget.handleGoTo),
-          BlockAppListComponent(
-              categoryId: 'home_popular',
-              appStreamList: statePopularAppStreamList,
-              appPath: appPath,
-              handleGoTo: widget.handleGoTo)
+          stateTrendingAppStreamList.isEmpty
+              ? getWait('home_trending')
+              : BlockAppListComponent(
+                  categoryId: 'home_trending',
+                  appStreamList: stateTrendingAppStreamList,
+                  appPath: appPath,
+                  handleGoTo: widget.handleGoTo),
+          stateUpdatedAppStreamList.isEmpty
+              ? getWait('home_updated')
+              : BlockAppListComponent(
+                  categoryId: 'home_updated',
+                  appStreamList: stateUpdatedAppStreamList,
+                  appPath: appPath,
+                  handleGoTo: widget.handleGoTo),
+          statePopularAppStreamList.isEmpty
+              ? getWait('home_popular')
+              : BlockAppListComponent(
+                  categoryId: 'home_popular',
+                  appStreamList: statePopularAppStreamList,
+                  appPath: appPath,
+                  handleGoTo: widget.handleGoTo)
         ]));
   }
 }
